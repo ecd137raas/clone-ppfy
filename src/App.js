2397418b502d7448da381065ed21a8df848da803
Emerson Duarte <raasjava@gmail.com>
2020-09-24 20:48:33 -0300

    Subindo projeto

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Globalstyle from './styles/global';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+     <Header />
+
+     <Globalstyle />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container } from './style';
+
+export default function Header(){
+    return(
+        <Container>
+            <h1>Clone Pipefy</h1>
+        </Container>
+    );
+}
+
